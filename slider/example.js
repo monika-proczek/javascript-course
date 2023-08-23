7c4f7bbaaa571b972124f0c7af44735d921fe0ad
Monika Proczek <monikaproczek9@gmail.com>
2023-08-23 02:20:37 +0200

    simple slide changing

---
+++ slider/example.js
@@ -0,0 +1,6 @@
+let myFirstFunction = (name, greeting) => {
+    console.log(greeting + ' mam na imię ' + name + '!');
+};
+
+myFirstFunction('Monika', 'Hej');
+myFirstFunction('Jan', 'Dzień dobry');
